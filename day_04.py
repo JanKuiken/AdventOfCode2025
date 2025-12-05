4126f0e28fbd4e53afe84f7c7bd1c3aaee68a3f4
Jan Kuiken <jan.kuiken@quicknet.nl>
2025-12-05 15:30:25 +0100

    finished day 4

---
+++ day_04.py
@@ -0,0 +1,52 @@
+"""
+Advent of Code 2025, day 4
+"""
+import aoc_lib as aoc
+from copy import deepcopy
+
+matrix = aoc.matrix_from_file('input_04.txt')
+
+aoc.TablePoint.max_row = len(matrix)
+aoc.TablePoint.max_col = len(matrix[0])
+
+total = 0
+for tp in aoc.TablePoint.iterate():
+    if matrix[tp.row][tp.col] == '@':
+        neighbours = tp.neighbours()
+        rolls = 0
+        for n in neighbours:
+            if matrix[n.row][n.col] == '@':
+                rolls += 1
+        if rolls < 4:
+            total += 1
+
+print('answer part 1 :', total)
+
+# part two
+
+def remove_rolls():
+    new_matrix = deepcopy(matrix)
+    total = 0
+    for tp in aoc.TablePoint.iterate():
+        if matrix[tp.row][tp.col] == '@':
+            neighbours = tp.neighbours()
+            rolls = 0
+            for n in neighbours:
+                if matrix[n.row][n.col] == '@':
+                    rolls += 1
+            if rolls < 4:
+                total += 1
+                new_matrix[tp.row][tp.col] = '.'
+
+    return total, new_matrix
+
+
+total_rolls_removed = 0
+removed = -1
+while removed != 0:
+    removed, matrix = remove_rolls()
+    total_rolls_removed += removed
+
+# 4413 is too low....
+print('answer part 2 :', total_rolls_removed)
+
